365ad464b5c20f4b07df2da61649b761caca21cf
ke4tri <ke4tri@gmail.com>
2020-02-13 17:28:53 -0600

    creating users works with postman and was able to use new collection in mongodb

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,8 +13,62 @@ router.get("/", function(req, res) {
   res.send("Birds home page");
 });
 
-router.post("/", function(req, res) {
-  res.send("Got a POST request USERS");
+// router.post("/", function(req, res) {
+//   res.send("Got a POST request USERS");
+// });
+
+router.post("/", async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body; //destructuring req.body.name
+
+  try {
+    // See if user exists
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+    }
+
+    user = new User({
+      name,
+      email,
+      password
+    });
+
+    // Encrypt password 10.. the more you have the more secure but slower
+    const salt = await bcrypt.genSalt(10);
+    // creates a hash and saves it to the db
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    // Return jsonwebtoken
+    // res.send("User registered");
+    //sub res.send with payload
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      config.get("jwtSecret"),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
